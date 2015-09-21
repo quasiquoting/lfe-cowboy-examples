84ffcfa0f9dee42713e49b5690d14baccd9aabf5
Eric Bailey <eric@ericb.me>
2015-09-20 22:17:08 -0500

    Add generic common.mk

---
+++ common.mk
@@ -0,0 +1,11 @@
+PROJECT := $(notdir $(CURDIR))
+DEPS = cowboy
+dep_cowboy = git https://github.com/ninenines/cowboy master
+include ../erlang.mk
+
+
+all::
+	mkdir -p ebin && lfec -o ebin src/*.lfe
+
+dev:
+	lfe -pa ebin deps/*/ebin -s $(PROJECT) start
